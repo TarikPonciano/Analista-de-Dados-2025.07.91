6c13d073401bd0070b8f4f8c8583db28e43c1f47
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-11-25 20:23:24 -0300

    Exemplos Aula 1

---
+++ Aula 1/primeira_aula.py
@@ -0,0 +1,22 @@
+# Imprimir "Olá, Maria!"
+# Imprimir "Seja bem vindo, Maria!"
+
+#nome_da_variavel Snake Case
+#nomeDaVariavel Camel Case
+
+# nome_cliente = "Maria"
+
+# print(f"Olá, {nome_cliente}!")
+# print(f"Seja bem vindo, {nome_cliente}!")
+
+# Crie um programa que imprime na tela o seguinte texto:
+# Olá, meu nome é {nome}! Tenho {idade} anos e {altura} metros!
+
+nome = "Tarik Ponciano"
+idade = 29
+altura = 1.7
+
+print(f"Olá, meu nome é {nome}! Tenho {idade} anos e {altura} metros!")
+
+# Imprima na tela a data de nascimento dessa pessoa com a mensagem a seguir. Considere o ano atual como 2025. Ignore se a pessoa já fez aniversário ou não:
+# "Nasci no ano {ano}!"
