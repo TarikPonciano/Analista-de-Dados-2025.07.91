2d589afb922246a646c33b9169080d6d091e5d27
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-11-27 19:06:27 -0300

    Update exemplo1.py

--- Aula 3/exemplo1.py
+++ Aula 3/exemplo1.py
@@ -1,9 +1,9 @@
 # Crie um programa que recebe a idade de um usuário. Categorize se a pessoa é jovem ou adulto seguindo o critério abaixo:
 # Menor que 18 anos, Jovem
-# Acima de 18 anos, Adulto
+# 18 anos ou mais, Adulto
 
 # Nível 2: Categorize agora em mais faixas de idade:
 
 # Menor que 18, Jovem
-# Acima de 18 e menor que 65, Adulto
+# 18 anos ou mais e menor que 65, Adulto
 # Acima de 65, Sênior
