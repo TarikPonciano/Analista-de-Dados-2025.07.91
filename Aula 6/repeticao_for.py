5b063178584a02d4d6e897f0cf472e6fd3d68b1a
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-12-03 20:02:53 -0300

    Create repeticao_for.py

---
+++ Aula 6/repeticao_for.py
@@ -0,0 +1,37 @@
+# Modifique o código abaixo para pedir ao usuário um nome, e imprimir o MESMO nome 5 vezes
+
+# Modifique o código para pedir um nome de usuário e depois imprimir "Olá, {nome}" na tela, repita esse processo 5 vezes.
+
+# for i in range(5):
+#     nome = input("Digite seu nome: ")
+#     print(f"{i}. Olá, {nome}!")
+
+
+# Faça um programa que exibe os números de 0 a 100
+
+# for i in range(101):
+#     print(i)
+
+# Faça um programa que exibe na tela os números pares de 1 até 1000
+
+# for i in range(1,1001):
+#     if i % 2 == 0:
+#         print(i)
+
+# Faça um programa que imprime a soma de 0 até 1000
+# soma = 0
+# for i in range(1001):
+#     soma += i
+
+# print(f"Resultado de 0 a 1000: {soma}")
+
+# Faça um programa que pede um número ao usuário e imprime a soma de 0 até o número escolhido
+
+# numero = int(input("Digite o número que deseja realizar a soma:"))
+# soma = 0
+# for i in range(numero+1):
+#     soma += i
+
+# print(f"Resultado de 0 a {numero}: {soma}")
+
+# Faça um programa que pede dez números ao usuário e imprime a soma desses números
