263bdab9fdb3195da29a881369bb4fceb5d7edd9
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-12-03 20:41:15 -0300

    Create questoes_for.py

---
+++ Aula 6/questoes_for.py
@@ -0,0 +1,12 @@
+# Crie um programa de cálculo de total de venda. Peça o valor de 5 produtos diferentes (somente o preço) e a quantidade comprada. Ao final exiba o valor total da compra.
+# Bônus: Peça o nome dos produtos e ao final exiba a nota fiscal da compra contendo Produto - Preço - Quantidade - Total
+
+# Crie um programa que pede 5 números ao usuário. Ao final determine e imprima o maior número digitado.
+# Bônus: Ao final exiba na tela todos os números que foram digitados
+
+# Crie um programa de cálculo de notas. O programa deverá pedir ao usuário as 4 notas de um aluno (usar for) e ao final deverá exibir média e situação do aluno.
+# >= 7 - Aprovado
+# >= 4 and < 7 - Recuperação
+# < 4 - Reprovado
+# Bônus: Peça ao usuário quantas notas ele deseja digitar. Utilize no cálculo de média somente notas válidas (0 a 10)
+
