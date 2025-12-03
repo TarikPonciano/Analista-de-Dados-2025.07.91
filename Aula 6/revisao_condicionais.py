0a7adb003d8e6ebb5eee77c40e2bad54d4d01fad
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-12-03 18:23:54 -0300

    Create revisao_condicionais.py

---
+++ Aula 6/revisao_condicionais.py
@@ -0,0 +1,3 @@
+# 1. Faça um Programa que leia três números e mostre-os em ordem decrescente.
+
+# 2. Faça um Programa para um caixa eletrônico. O programa deverá perguntar ao usuário a valor do saque e depois informar quantas notas de cada valor serão fornecidas. As notas disponíveis serão as de 1, 5, 10, 50 e 100 reais. O valor mínimo é de 10 reais e o máximo de 600 reais. O programa não deve se preocupar com a quantidade de notas existentes na máquina, mas deverá validar se o valor inserido está no intervalo descrito.
