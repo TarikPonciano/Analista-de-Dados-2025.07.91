088abb546e1e8fe91f38039f3a154759b267999c
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-12-05 20:21:47 -0300

    Update intro_while.py

--- Aula 8/intro_while.py
+++ Aula 8/intro_while.py
@@ -24,11 +24,18 @@
 
 # Crie um programa que recebe um número e depois exibe a tabuada desse número. (USANDO WHILE!)
 
-numero = int(input("Digite um número: "))
+# numero = int(input("Digite um número: "))
 
-multiplicador = 1
+# multiplicador = 1
 
-while multiplicador <= 10:
+# while multiplicador <= 10:
 
-    print(f"{numero} X {multiplicador} = {numero*multiplicador}")
-    multiplicador += 1
+#     print(f"{numero} X {multiplicador} = {numero*multiplicador}")
+#     multiplicador += 1
+
+# Crie um programa que recebe números inteiros, até que a soma desses números seja igual a 100. 
+# Fraseamento alternativo: Enquanto a soma dos números for menor que 100, receba novos números inteiros.
+
+# Crie um programa que exibe os 10 primeiros números divisíveis por 6.
+
+# Crie um programa que pede um palpite (0 a 100) para um jogador e repete caso o jogador erre o número secreto. Se o jogador acertar, o programa é encerrado e é exibido na tela, quantos palpites foram necessários para acertar.
