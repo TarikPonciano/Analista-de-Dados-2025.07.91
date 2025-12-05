d62e612d6a73b9c042acc34d4e1f09e5432fc1b1
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-12-05 19:41:48 -0300

    Update aquecimento.py

--- Aula 8/aquecimento.py
+++ Aula 8/aquecimento.py
@@ -5,16 +5,33 @@ qtd_candidatos = int(input("Digite a quantidade de candidatos: "))
 total_aprovados = 0
 total_reprovados = 0
 soma_notas = 0
+historico = ""
 
 for i in range(qtd_candidatos):
     nome = input(f"Digite o nome do candidato {i+1}:")
 
     nota = int(input(f"Digite a nota (0-100) do candidato {i+1}:"))
 
+    soma_notas += nota
+
     #Verificar se foi aprovado e imprimir a mensagem correspondente
 
     if nota >= 80:
         print(f"Candidato {i+1}: {nome} - Aprovado")
         total_aprovados += 1
+
+        historico += f"{i+1}. {nome} - {nota} - Aprovado\n"
     else:
         print(f"Candidato {i+1}: {nome} - Reprovado")
+        total_reprovados += 1
+        historico += f"{i+1}. {nome} - {nota} - Reprovado\n"
+
+print("Lista de Participantes:")
+print(historico)
+
+
+print("------------RESUMO DO CONCURSO------------")
+print("Aprovados:", total_aprovados)
+print("Reprovados:", total_reprovados)
+print("Média Geral:", round((soma_notas/qtd_candidatos),2))
+# print(f"Média Geral: {round((soma_notas/qtd_candidatos), 2)}")
