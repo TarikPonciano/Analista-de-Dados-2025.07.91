d3625b8bc213d5e8cd70def285dfc9d21f1ac30e
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-12-05 20:16:55 -0300

    Create intro_while.py

---
+++ Aula 8/intro_while.py
@@ -0,0 +1,34 @@
+# contador = 0
+
+# while contador < 5:
+#     print(contador)
+#     contador += 1
+
+# Crie um programa (usando while) que exibe na tela todos os números de 0 a 100
+
+# contador = 0
+
+# while contador <= 100:
+#     print(contador)
+#     contador += 1
+
+# Crie um programa (usando while) que exibe apenas os números impares de 0 a 200
+
+# contador = 0
+
+# while contador <= 200:
+#     if contador % 2 != 0:
+#         print(contador)
+    
+#     contador += 1
+
+# Crie um programa que recebe um número e depois exibe a tabuada desse número. (USANDO WHILE!)
+
+numero = int(input("Digite um número: "))
+
+multiplicador = 1
+
+while multiplicador <= 10:
+
+    print(f"{numero} X {multiplicador} = {numero*multiplicador}")
+    multiplicador += 1
