593d40a8e6ac4794cecf391a94f774ff5475bcfd
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-12-11 19:17:55 -0300

    Update intro_colecoes.py

--- Aula 11/intro_colecoes.py
+++ Aula 11/intro_colecoes.py
@@ -1,21 +1,32 @@
-texto = "Abacate"
+# texto = "Abacate"
 
-print("Primeira letra:",texto[0])
-#Crie uma variável contendo uma palavra, imprima na tela apenas a última letra
-print("Última letra:", texto[-1])
-print("Letra do meio:", texto[int(len(texto)/2)])
+# print("Primeira letra:",texto[0])
+# #Crie uma variável contendo uma palavra, imprima na tela apenas a última letra
+# print("Última letra:", texto[-1])
+# print("Letra do meio:", texto[int(len(texto)/2)])
 
-print("Tamanho do texto:", len(texto))
+# print("Tamanho do texto:", len(texto))
 
-for i in range(len(texto)):
-    letra = texto[i]
+# for i in range(len(texto)):
+#     letra = texto[i]
 
-    # if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
-    #     print(letra)
+#     # if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+#     #     print(letra)
 
-    if letra in "aeiou":
-        print(letra)
+#     if letra.upper() in "aeiouAEIOU":
+#         print(letra)
 
 
 # Crie um programa que pede 5 nomes de cachorro. Contabilize quantos nomes começam com vogal. Ao final exiba a contagem de nomes:
+
+qtd_vogais = 0
+
+for i in range(5):
+    nome = input("Digite um nome de pet: ")
+
+    if nome[0] in "aeiouAEIOU":
+        qtd_vogais += 1
+
+print(f"Quantidade de nomes que começam com vogal: {qtd_vogais}")
+
     
