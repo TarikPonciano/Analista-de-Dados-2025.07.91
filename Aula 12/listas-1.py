18f492e9d9cb6c29c48d3c4ea72f2b839b3d9b2f
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-12-12 19:22:58 -0300

    Ajuste listas

--- Aula 12/listas-1.py
+++ Aula 12/listas-1.py
@@ -1,1 +1,3 @@
 # Crie um programa que pede a idade de 10 pessoas. Armazene essas idades em uma lista e calcule a média de idade, a menor idade, a maior idade e quantas pessoas possuem idade maior ou igual a 30 anos.
+# Crie uma lista antes das repetições
+# Use o comando .append para adicionar elementos na lista
