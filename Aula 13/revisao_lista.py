5eb7e58f02a7c0b3ededc439c8635deae18028c8
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-12-15 18:38:43 -0300

    Lista

---
+++ Aula 13/revisao_lista.py
@@ -0,0 +1,10 @@
+# 1. Crie um programa que pede o nome de 5 frutas e ao final imprime uma lista numerada com o nome das frutas. Exemplo:
+
+# 1. Fruta 1
+# 2. Fruta 2
+# 3. Fruta 3
+# 4. Fruta 4
+
+# Continue o programa anterior e agora ao lado do nome de cada fruta acrescente o texto "é saudável!"
+
+# 2. Crie um programa que pede o nome de um aluno, seu cpf e idade além de 4 notas VÁLIDAS (0 a 10) desse aluno. Ao final imprima uma ficha com as informações pessoais desse aluno, sua média, situação(aprovado, recuperação)
