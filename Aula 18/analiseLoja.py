b17ac717d998adba2552eb0e8892216366f82252
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-01-07 19:26:43 -0300

    Update analiseLoja.py

--- Aula 18/analiseLoja.py
+++ Aula 18/analiseLoja.py
@@ -9,3 +9,10 @@ for venda in listaVendas:
     totalUnidades += venda["quantidade"]
 
 print(f"Total de Unidades Vendidas: {totalUnidades}")
+
+# Produzir as métricas e exibi-las ao usuário:
+
+# 1. Total e Média das vendas gerais
+# 2. Total das vendas por categoria
+# 3. Total das vendas por vendedor
+# 4. Total das vendas por região
