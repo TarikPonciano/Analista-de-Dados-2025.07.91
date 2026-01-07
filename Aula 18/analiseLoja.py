4261c0d41d2b106c67580378620ecddaa87fd3f3
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-01-07 20:36:07 -0300

    Update analiseLoja.py

--- Aula 18/analiseLoja.py
+++ Aula 18/analiseLoja.py
@@ -24,11 +24,35 @@ for venda in lista_vendas:
     total_geral += venda["quantidade"] * venda["valor_unitario"]
 
 media_geral = total_geral/len(lista_vendas)
+
 print(f"Total das vendas: R$ {total_geral:.2f}")
 print(f"Ticket Médio: R$ {media_geral}")
 print(f"Quantidade de Vendas: {len(lista_vendas)}")
+
 #Exibir total de unidades vendidas
+total_unidades = 0
+for venda in lista_vendas:
+    total_unidades += venda["quantidade"]
+
+print(f"Total de Unidades Vendidas: {total_unidades}")
+
 #Maior e Menor Venda
+maior_venda = lista_vendas[0]["valor_unitario"] * lista_vendas[0]["quantidade"] 
+menor_venda = lista_vendas[0]["valor_unitario"] * lista_vendas[0]["quantidade"] 
+
+for venda in lista_vendas:
+    total_venda = venda["quantidade"] * venda["valor_unitario"]
+
+    if total_venda > maior_venda:
+        maior_venda = total_venda
+
+    if total_venda < menor_venda:
+        menor_venda = total_venda
+
+print(f"Maior Valor de Venda: R$ {maior_venda}")
+print(f"Menor Valor de Venda: R$ {menor_venda}")
+
+#Exibir a quantidade de vendas que superou a meta de venda de 5000 reais
 
 #Exibir o nome dos Vendedores
 
