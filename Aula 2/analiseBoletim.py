baf7f9ffaf6124bc08c1b004357863bf5b4494a1
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-02-02 20:30:38 -0300

    Update analiseBoletim.py

--- Aula 2/analiseBoletim.py
+++ Aula 2/analiseBoletim.py
@@ -23,9 +23,50 @@ dados["media"] = (dados["nota_b1"] + dados["nota_b2"] + dados["nota_b3"] + dados
 #Maneira funcional
 # dados["media"] = dados[["nota_b1", "nota_b2", "nota_b3", "nota_b4"]].mean(axis=1)
 
-dados["Identificador"] = dados["nome"] + " - " + dados["turma"]
+dados["identificador"] = dados["nome"] + " - " + dados["turma"]
 
 # Produzir a coluna Situação informando a situação de cada aluno
 
+# Aprovado -> 7 a 10
+# Recuperação -> 4 a 7
+# Reprovado -> 0 a 4
 
+dados["situacao"] = "Reprovado"
 
+dados.loc[(dados["media"] >= 4) & (dados["media"] < 7), "situacao"] = "Recuperação"
+
+dados.loc[(dados["media"] >= 7) & (dados["media"] <= 10), "situacao"] = "Aprovado"
+
+# Exibir 3 seções distintas. Tabela para aprovados, tabela para reprovados e tabela para recuperação
+
+#Lista de Aprovados
+aprovados = dados[dados["situacao"]=="Aprovado"]
+
+print("Lista de Aprovados:")
+
+print(aprovados[["identificador", "media","situacao"]])
+
+#Lista de Recuperação
+recuperacao = dados[dados["situacao"]=="Recuperação"]
+
+print("Lista de Recuperação:")
+
+print(recuperacao[["identificador", "media","situacao"]])
+
+#Lista de Recuperação
+reprovados = dados[dados["situacao"]=="Reprovado"]
+
+print("Lista de Reprovados:")
+
+print(reprovados[["identificador", "media","situacao"]])
+
+# Mostre os 5 melhores alunos
+# Mostre os 5 piores alunos 
+
+dados_ordenado = dados.sort_values("media", ascending=False)
+
+print("TOP 5 Alunos")
+print(dados_ordenado[["nome", "media"]].head(5))
+
+print("BOTTOM 5 Alunos")
+print(dados_ordenado[["nome","media"]].tail(5))
