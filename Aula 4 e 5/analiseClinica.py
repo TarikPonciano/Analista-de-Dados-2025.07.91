78c90d28d823f15a1a470d66490084e8a92d02d3
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-02-05 20:36:33 -0300

    Update analiseClinica.py

--- Aula 4 e 5/analiseClinica.py
+++ Aula 4 e 5/analiseClinica.py
@@ -16,11 +16,30 @@ dados = pd.read_excel("dados_clinica_simplificado.xlsx", engine="openpyxl")
 # print(dados.isnull().sum())
 
 # Missão 2 - Padronização dos nomes
-dados["Nome"] = dados["Nome"].astype("str").str.split().str.title()
+dados["Nome"] = dados["Nome"].astype("str").str.strip().str.title()
 
 # Missão 3 - Padronizar idade
 
 
+dados["Idade"] = dados["Idade"].replace(["", " ", "N/A"], pd.NA)
 
-print(dados.sample(10))
+dados["Idade"] = pd.to_numeric(dados["Idade"], errors="coerce")
+
+# Se a coluna for extremamente importante, faz sentido filtrar os dados ruins
+
+# dados_limpo = dados[dados["Idade"].notna()]
+
+mediana_idade = dados[dados["Idade"].notna()]["Idade"].median()
+
+dados.loc[dados["Idade"].isna(), "Idade"] = mediana_idade
+
+dados["Idade"] = dados["Idade"].astype("int64")
+
+dados["Idade"] = dados["Idade"].clip(lower=0, upper=120)
+
+# print(dados[(dados["Idade"] < 0) | (dados["Idade"] > 120)])
+
+
+# print(mediana_idade)
+print(dados["Sexo"].value_counts())
 
