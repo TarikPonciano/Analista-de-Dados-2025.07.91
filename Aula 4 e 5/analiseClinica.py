a3fa2d0b5dd2ff71fae4c385e6c7e0e5a621d587
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-02-05 20:01:06 -0300

    Create analiseClinica.py

---
+++ Aula 4 e 5/analiseClinica.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+dados = pd.read_excel("dados_clinica_simplificado.xlsx", engine="openpyxl")
+
+# Missão 1 - Diagnóstico dos dados
+
+# print(dados.head(10))
+
+# print("Dimensões")
+# print(f"Linhas: {dados.shape[0]} Colunas: {dados.shape[1]}")
+
+# print("Tipos de dados das colunas")
+# print(dados.dtypes)
+
+# print("Dados nulos por coluna")
+# print(dados.isnull().sum())
+
+# Missão 2 - Padronização dos nomes
+dados["Nome"] = dados["Nome"].astype("str").str.split().str.title()
+
+# Missão 3 - Padronizar idade
+
+
+
+print(dados.sample(10))
+
