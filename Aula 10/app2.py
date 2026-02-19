e6d1069403538bc6359ef6dddd618b0c9eedfd88
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-02-19 20:13:12 -0300

    Update app2.py

--- Aula 10/app2.py
+++ Aula 10/app2.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 dados = {
     "meses": ["Janeiro", "Fevereiro", "Março", "Abril" , "Maio", "Junho"],
-    "vendas": [3000, 3500 , 4000 , 1200, 1600, 3500]
+    "vendas": [3000, 3500 , 4000 , 1200, 1600, 3500],
+    "lojas": ["Loja C", "Loja B", "Loja A", "Loja B", "Loja C", "Loja C"]
 }
 
 df = pd.DataFrame(dados)
@@ -16,3 +17,21 @@ st.title("Primeira Tabela Streamlit")
 st.divider()
 
 st.dataframe(df)
+st.divider()
+
+st.header("Versão Filtrada por Loja")
+
+#loja = st.text_input("Digite o nome da loja:")
+
+lojas = ["Todos"] + list(df["lojas"].unique())
+
+loja = st.selectbox("Escolha uma loja: ", lojas)
+
+dfLojas = df[df["lojas"]==loja]
+
+if loja == "Todos":
+    st.dataframe(df)
+else:
+    st.dataframe(dfLojas)
+
+# Produzir uma tabela (groupby) que exibe o faturamento total por loja
