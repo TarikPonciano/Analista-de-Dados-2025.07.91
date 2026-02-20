ba12c480782bb7025af030d4a6f8993b00b69063
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-02-20 19:43:17 -0300

    Update appSimples.py

--- Aula 11/appSimples.py
+++ Aula 11/appSimples.py
@@ -3,6 +3,8 @@
 
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # Expandir os dados com uma coluna lojas (3 lojas diferentes)
 
@@ -55,4 +57,23 @@ st.metric(f"Percentual de Participação da {loja}", f"{((faturamento_filtrado/f
 
 st.metric("Faturamento Geral", f"R$ {faturamento_geral:,.2f}")
 
+st.divider()
+
+# Produzir um gráfico de vendas por mês
+st.header("Gráficos")
+
+# Cria a "figura" do gráfico
+fig, ax = plt.subplots()
+
+# Usar o ax para configurar o gráfico 
+ax.plot(df["meses"], df["vendas"], marker="o")
+
+ax.set_ylim(bottom=0, top=df["vendas"].max()+500)
+ax.set_xlabel("Meses")
+ax.set_ylabel("Faturamento")
+ax.set_title("Faturamento por Mês")
+ax.grid(True)
+
+# Pede para o streamlit exibir a "figura"
+st.pyplot(fig)
 
