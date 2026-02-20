b47a7def9d85c1401f94354fdfb4e0140cd3ffc8
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-02-20 19:08:52 -0300

    Create appSimples.py

---
+++ Aula 11/appSimples.py
@@ -0,0 +1,51 @@
+# Para rodar a página executar no servidor:
+# streamlit run appSimples.py
+
+import streamlit as st
+import pandas as pd
+
+# Expandir os dados com uma coluna lojas (3 lojas diferentes)
+
+# Permitir que a pessoa digite o nome da loja desejado e exiba na tabela somente os meses que tem vendas da loja escolhida
+
+dados = {
+    "meses": ["Janeiro", "Fevereiro", "Março", "Abril" , "Maio", "Junho"],
+    "vendas": [3000, 3500 , 4000 , 1200, 1600, 3500],
+    "lojas": ["Loja C", "Loja B", "Loja A", "Loja B", "Loja C", "Loja C"]
+}
+
+df = pd.DataFrame(dados)
+
+st.title("Primeira Tabela Streamlit")
+st.divider()
+
+st.dataframe(df)
+st.divider()
+
+st.header("Versão Filtrada por Loja")
+
+#loja = st.text_input("Digite o nome da loja:")
+
+lojas = ["Todos"] + df["lojas"].unique().tolist()
+
+loja = st.selectbox("Escolha uma loja: ", lojas)
+
+dfLojas = df.copy()[df["lojas"] == loja]
+
+# Produzir uma tabela (groupby) que exibe o faturamento total por loja
+
+df_faturamento_loja = df.groupby("lojas").agg(
+    faturamento_total=("vendas", "sum")
+).reset_index()
+
+if loja == "Todos":
+    st.dataframe(df)
+    st.dataframe(df_faturamento_loja)
+else:
+    st.dataframe(dfLojas)
+    st.dataframe(df_faturamento_loja.copy()[df_faturamento_loja["lojas"] == loja])
+
+# Exibir na tela do nosso streamlit o Faturamento Total Geral
+
+
+
