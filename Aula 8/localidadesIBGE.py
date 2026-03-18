ba1e92e3c5b555f3749cd12fe9ef32c29603bfd8
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-03-18 19:53:30 -0300

    Create localidadesIBGE.py

---
+++ Aula 8/localidadesIBGE.py
@@ -0,0 +1,4 @@
+# https://servicodados.ibge.gov.br/api/v1/localidades/estados
+
+# Faça uma requisição ao endpoint de estados do IBGE
+# A partir do resultado da requisição imprima o nome de todos os estados do Brasil
