27c12208179ee379aa209322e60874702d62c131
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-03-18 20:16:25 -0300

    Update localidadesIBGE.py

--- Aula 8/localidadesIBGE.py
+++ Aula 8/localidadesIBGE.py
@@ -2,3 +2,14 @@
 
 # Faça uma requisição ao endpoint de estados do IBGE
 # A partir do resultado da requisição imprima o nome de todos os estados do Brasil
+
+# Imprima agora para cada estado "Nome - Região"
+
+import requests
+
+resposta = requests.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+
+listaEstados = resposta.json()
+
+for estado in listaEstados:
+    print(estado["nome"])
