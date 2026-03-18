4139335447bfadacf07d6c68ba9f2c5505bf9ab0
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-03-18 20:47:39 -0300

    Aula 8

--- Aula 8/localidadesIBGE.py
+++ Aula 8/localidadesIBGE.py
@@ -6,10 +6,33 @@
 # Imprima agora para cada estado "Nome - Região"
 
 import requests
+import pandas as pd
 
 resposta = requests.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
 
 listaEstados = resposta.json()
 
+listaEstadosFormatado = []
+
 for estado in listaEstados:
-    print(estado["nome"])
+    
+    idEstado = estado["id"]
+    nome = estado["nome"]
+    uf = estado["sigla"]
+    regiao = estado["regiao"]["nome"]
+
+    estadoFormatado = {
+        "id": idEstado,
+        "nome": nome,
+        "uf": uf,
+        "regiao": regiao
+    }
+
+    listaEstadosFormatado.append(estadoFormatado)
+
+
+df = pd.DataFrame(listaEstadosFormatado)
+
+df = df.sort_values("id", ascending=True)
+
+df.to_excel("tabela_estados.xlsx", index=False)
